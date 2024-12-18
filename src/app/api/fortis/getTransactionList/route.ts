fd3e886e658594220835bddb4af09195bfa6eb6e
Kishan <kishan.excel2011@gmail.com>
2024-12-18 14:40:06 +0530

    code added

--- src/app/api/fortis/getTransactionList/route.ts
+++ src/app/api/fortis/getTransactionList/route.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-    runtime: 'edge',
-};
-
 export async function GET(req: NextRequest) {
     try {
         // Forward the request to the Fortis API
